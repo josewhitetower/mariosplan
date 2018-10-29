d345578b1b5a9b3cf5dbce36265fdbb5779b94b2
Jose Torreblanca <josewhitetower@gmail.com>
2018-10-30 00:24:25 +0100

    add signin nd signout

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+import  {connect} from 'react-redux';
+import {signIn} from '../../store/actions/authActions';
+
 class SignIn extends Component {
 
     state = {
@@ -14,11 +17,11 @@ class SignIn extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+       this.props.signIn(this.state)
     }
   render() {
+    const {authError} = this.props;
     return (
-     
         <div className="container">
             <form className="white" onSubmit={this.handleSubmit}>
                 <h5 className="grey-text text-darken-3">Sign In</h5>
@@ -30,12 +33,27 @@ class SignIn extends Component {
                     <label htmlFor="password">Password</label>
                     <input type="password" name="password" id="password" onChange={this.handleChange}/>
                 </div>
-                <div className="input-field"><button className="btn pink lighten-1 z-depth-0">Login</button></div>
+                <div className="input-field">
+                    <button className="btn pink lighten-1 z-depth-0">Login</button>
+                    <div className="red-text center">
+                        {authError ? <p>{authError}</p> : null}
+                    </div>
+                </div>
             </form>
         </div>
       
     )
   }
 }
+const mapStateToProps = (state) => {
+    return {
+        authError:state.auth.authError
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return  {
+        signIn: (credentials) => dispatch(signIn(credentials))
+    }
+}
 
-export default SignIn
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,12 +3,15 @@ import projectReducer from './projectReducer'
 
 import { firestoreReducer} from 'redux-firestore'
 
+import { firebaseReducer}  from 'react-redux-firebase'
+
 import {combineReducers} from 'redux';
 
 const rootReducer =  combineReducers({
     auth: authReducer,
     project: projectReducer,
-    firestore: firestoreReducer
+    firestore: firestoreReducer,
+    firebase: firebaseReducer
 })
 
 export default rootReducer;
